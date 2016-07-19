5228218047be7151a41a89c1af3149805a28e5ae
aniruddh-capital <aniruddh.capital@gmail.com>
2016-07-19 11:44:56 +0530

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "";
-$mysql_password = "";
-$mysql_database = "sitedatabase";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Oops something went wrong");
-mysql_select_db($mysql_database, $bd) or die("Oops something went wrong");
+  $mysql_hostname = "localhost";
+  $mysql_user = "";
+  $mysql_password = "";
+  $mysql_database = "sitedatabase";
+  $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Oops something went wrong");
+  mysql_select_db($mysql_database, $bd) or die("Oops something went wrong");
 ?>
